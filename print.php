e755955bf8b68b53c2ecb8fc778dce87240f54bc
vinay manyam <vinayip541@gmail.com>
2017-08-11 04:47:35 +0530

    Add files via upload

---
+++ print.php
@@ -0,0 +1,27 @@
+<html> 
+   <head> 
+     
+  
+    
+      <script type="text/javascript" language="javascript"> 
+    function printDiv(divName) {
+     var printContents = document.getElementById(divName).innerHTML;
+     var originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+    window.print();
+     document.body.innerHTML = originalContents;
+}
+   </script> 
+   </head> 
+  
+   <body>  
+      
+<div id="printableArea">
+      <h1>Print me</h1>
+</div>
+
+<input type="button" onclick="printDiv('printableArea')" value="print a div!" />
+       
+       
+   </body> 
+</html>
